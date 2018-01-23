81b93aea83348075d3cb4d4a372a871264cfb261
Admin <git@webgk.ru>
2018-01-23 15:50:01 +0300

    add folder in top_menu/sorevnovaniya/

---
+++ sorevnovaniya/.left.menu.php
@@ -0,0 +1,12 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$aMenuLinks = Array(
+	Array(
+		"Результаты", 
+		"rezultaty/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
